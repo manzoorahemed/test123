4839d79e76441f2c00ed36acce8cae820ebe2b9c
manzoorahmed <manzoorahmed821@gmail.com>
2023-12-08 13:14:20 +0500

    t

--- test123/UserControl1.Designer.cs
+++ test123/UserControl1.Designer.cs
@@ -31,6 +31,7 @@
             components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            //adfds 
         }
 
         #endregion
